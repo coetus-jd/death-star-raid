79f7cf1f1a5a8608f24efb97300031a1e9e2ff61
WaterWhirl <leonardo-zende@hotmail.com>
2022-07-01 16:39:27 -0300

    feat: add screen system
    In need to modify and readjust the code

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import scenario from "./components/scenario.js";
 import bullet from "./components/bullet.js";
 import enemy from "./components/enemy.js";
 import { Log } from "./utils/log.js";
+import screens from "./components/screens.js";
 
 /** Starts the game only when the DOM is fully loaded */
 document.addEventListener("DOMContentLoaded", awake);
