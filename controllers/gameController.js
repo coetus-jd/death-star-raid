5cf34711454d1aa7bd1a271d508dee9d2750d0eb
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-07-04 18:16:49 -0300

    fix import

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,20 +1,20 @@
-import player from "../components/player.js"
+import player from "../components/player.js";
 import GAME_SETTINGS from "../constants/gameSettings.js";
 import PLAYER_STATE from "../constants/playerState.js";
+import GAME_STATES from "../constants/gameStates.js";
 
 import { Log } from "../utils/log.js";
 
 export default {
-    checkIfPlayerHasLost() {
-        var delay = 500;
+  checkIfPlayerHasLost() {
+    var delay = 500;
 
-        if (player.life > 0) return;
+    if (player.life > 0) return;
 
-        setTimeout(function() {
-            GAME_SETTINGS.CURRENT_GAME_STATE = GAME_STATES.LOST;
-            player.state = PLAYER_STATE.DEAD;
-            Log.warn('Player has lost');
-          }, delay);
-
-    }
-}
+    setTimeout(function () {
+      GAME_SETTINGS.CURRENT_GAME_STATE = GAME_STATES.LOST;
+      player.state = PLAYER_STATE.DEAD;
+      Log.warn("Player has lost");
+    }, delay);
+  },
+};
