ec96b9dd0399fdad64483179fe3abfc7256fa849
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-07-05 22:07:16 -0300

    chore: add comments on log.js

--- utils/log.js
+++ utils/log.js
@@ -1,18 +1,37 @@
 import GAME_SETTINGS from "../constants/gameSettings.js";
 
+/**
+ * Class to log on console based on configurations
+ * @author Fabrício Pinto Ferreira
+ */
 export class Log {
+    /**
+     * Create a message on console with debug purpose
+     * @param {string} messages 
+     * @returns {void}
+     */
     static debug(messages) {
         if (!GAME_SETTINGS.LOGS.DEBUG_ENABLED) return;
 
         console.debug(messages);
     }
 
+    /**
+     * Create a message on console with warning purpose
+     * @param {string} messages 
+     * @returns {void}
+     */
     static warn(messages) {
         if (!GAME_SETTINGS.LOGS.WARN_ENABLED) return;
 
         console.warn(messages);
     }
 
+    /**
+     * Create a message on console with error purpose
+     * @param {string} messages 
+     * @returns {void}
+     */
     static error(messages) {
         if (!GAME_SETTINGS.LOGS.ERROR_ENABLED) return;
 
