a1b17ec7f286e7af68ecbffb37756127f1f753e5
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-07-09 19:55:16 -0300

    refact enemy component

--- components/enemy.js
+++ components/enemy.js
@@ -12,32 +12,35 @@ import bullet from "./bullet.js";
 import player from "./player.js";
 import animation from "../utils/animation.js";
 
-const baseWidth = 150;
-const baseHeight = 150;
-const baseLife = 2;
+const baseEnemyWidth = 150;
+const baseEnemyHeight = 150;
+const baseEnemyLife = 2;
 
 /** @type Utility */
 let utility = null;
 
+/**
+ * Enemy settings and behaviors
+ */
 export default {
   gravity: GAME_SETTINGS.GRAVITY,
   maxVelocity: GAME_SETTINGS.MAX_VELOCITY - 1,
-  /** @type {import('../types.js').Tile[]} */
+  /** @type {import('../types.js').Enemy[]} */
   enemies: [],
   animationFrame: 0,
   /** All positions that a enemy can be instantiated */
   possiblesPositions: [
     {
       x: GAME_SETTINGS.LIMIT_IN_X.MIN,
-      y: -baseHeight - 50,
+      y: -baseEnemyHeight - 50,
     },
     {
-      x: GAME_SETTINGS.LIMIT_IN_X.MIN + baseWidth,
-      y: -baseHeight - 25,
+      x: GAME_SETTINGS.LIMIT_IN_X.MIN + baseEnemyWidth,
+      y: -baseEnemyHeight - 25,
     },
     {
-      x: GAME_SETTINGS.LIMIT_IN_X.MIN + baseWidth * 2,
-      y: -baseHeight - 10,
+      x: GAME_SETTINGS.LIMIT_IN_X.MIN + baseEnemyWidth * 2,
+      y: -baseEnemyHeight - 10,
     },
   ],
   /**
@@ -93,7 +96,7 @@ export default {
   create: function () {
     if (this.enemies.length !== 0) return;
 
-    // Reset the velocity to the enemy "run"
+    // Positive velocity to the enemy "run" from the player
     this.maxVelocity = 3;
 
     /** @type {import('../types.js').Position} */
@@ -116,17 +119,14 @@ export default {
     const enemy1 = {
       x: randomPosition1.x,
       y: randomPosition1.y,
-      width: baseWidth,
-      height: baseHeight,
+      width: baseEnemyWidth,
+      height: baseEnemyHeight,
       velocityInY: 0,
-      life: baseLife,
+      life: baseEnemyLife,
       type: ENEMIES_TYPES.X_WING,
       state: ENEMY_STATES.MOVING_FORWARD,
       currentAnimationFrame: 0,
       imageSource: utility.getRandomImage("assets/Enemies/X-Wing", 6),
-      /**
-       * @returns {import('../types.js').Collider}
-       */
       getBoxCollider() {
         return {
           x: this.x + 30,
@@ -141,17 +141,14 @@ export default {
     const enemy2 = {
       x: randomPosition2.x,
       y: randomPosition2.y,
-      width: baseWidth,
-      height: baseHeight,
+      width: baseEnemyWidth,
+      height: baseEnemyHeight,
       velocityInY: 0,
       state: ENEMY_STATES.MOVING_FORWARD,
-      life: baseLife,
+      life: baseEnemyLife,
       type: ENEMIES_TYPES.Y_WING,
       currentAnimationFrame: 0,
       imageSource: utility.getRandomImage("assets/Enemies/Y-Wing", 4),
-      /**
-       * @returns {import('../types.js').Collider}
-       */
       getBoxCollider() {
         return {
           x: this.x + 40,
@@ -166,54 +163,60 @@ export default {
   },
   update: function () {
     this.enemies.forEach((enemy, index) => {
-      if (enemy.y > 0) this.maxVelocity = -1;
+        if (enemy.y > 0) this.maxVelocity = -1;
 
-      if (!enemy.velocityInY) enemy.velocityInY = this.maxVelocity;
-      if (!enemy.y) enemy.y = 0;
+        if (!enemy.velocityInY) enemy.velocityInY = this.maxVelocity;
+        if (!enemy.y) enemy.y = 0;
 
-      enemy.y += enemy.velocityInY;
+        enemy.y += enemy.velocityInY;
 
-      if (enemy.state === ENEMY_STATES.DEAD) return;
+        if (enemy.state === ENEMY_STATES.DEAD) return;
 
-      if (enemy.y - enemy.height > GAME_SETTINGS.BASE_HEIGHT) {
-        scoreController.addPoint();
-        removeEnemy.call(this, enemy, index);
+        if (enemy.y - enemy.height > GAME_SETTINGS.BASE_HEIGHT) {
+          scoreController.addPoint();
+          removeEnemy.call(this, enemy, index);
 
-        return;
-      }
-
-      if (utility.hasCollided(player, enemy)) {
-        Log.debug(`Enemy ${index} collide with player`);
-
-        removeEnemy.call(this, enemy, index);
-
-        player.takeDamage();
-        gameController.checkIfPlayerHasLost();
-
-        return;
-      }
-
-      bullet.bullets.forEach((bulletTile, bulletIndex) => {
-        if (!utility.hasCollided(bulletTile, enemy)) return;
-
-        Log.debug(`Bullet ${bulletIndex} collide with enemy`);
-
-        enemy.state = ENEMY_STATES.DAMAGE;
-        enemy.life--;
-        enemy.currentAnimationFrame = 0;
-
-        if (enemy.life <= 0) {
-          enemy.state = ENEMY_STATES.DEAD;
-          utility.clearRectUtil(enemy.x, enemy.y, enemy.width, enemy.height);
+          return;
         }
 
-        removeBullet(bulletTile, bulletIndex);
-        scoreController.addPoint();
-      });
-    });
+        if (utility.hasCollided(player, enemy)) {
+          Log.debug(`Enemy ${index} collide with player`);
+
+          removeEnemy.call(this, enemy, index);
+
+          player.takeDamage();
+          gameController.checkIfPlayerHasLost();
+
+          return;
+        }
+
+        bullet.bullets.forEach((bulletTile, bulletIndex) => {
+          if (!utility.hasCollided(bulletTile, enemy)) return;
+
+          Log.debug(`Bullet ${bulletIndex} collide with enemy`);
+
+          enemy.state = ENEMY_STATES.DAMAGE;
+          enemy.life--;
+          enemy.currentAnimationFrame = 0;
+
+          if (enemy.life <= 0) {
+            enemy.state = ENEMY_STATES.DEAD;
+            utility.clearRectUtil(enemy.x, enemy.y, enemy.width, enemy.height);
+          }
+
+          removeBullet(bulletTile, bulletIndex);
+          scoreController.addPoint();
+        });
+      }
+    );
   },
 };
 
+/**
+ * Remove a bullet from the canvas and its control array
+ * @param {import('../types.js').Tile} bulletTile 
+ * @param {number} bulletIndex 
+ */
 function removeBullet(bulletTile, bulletIndex) {
   utility.clearRectUtil(
     bulletTile.x,
@@ -224,6 +227,11 @@ function removeBullet(bulletTile, bulletIndex) {
   bullet.bullets.splice(bulletIndex, 1);
 }
 
+/**
+ * Remove an enemy from the canvas and its control array
+ * @param {import('../types.js').Tile} enemy 
+ * @param {number} index 
+ */
 function removeEnemy(enemy, index) {
   utility.clearRectUtil(enemy.x, enemy.y, enemy.width, enemy.height);
   this.enemies.splice(index, 1);
@@ -238,6 +246,10 @@ const explosionAnimations = [
   "assets/Damage/Explosão/0005.png",
 ];
 
+/**
+ * Play enemy's explosion animation
+ * @returns {void}
+ */
 function animateEnemyExplosion(enemy, index) {
   animation.animate(
     "enemyExplosion",
@@ -258,6 +270,10 @@ const enemyYWingIdleAnimations = [
   "assets/Enemies/Y-Wing/0003.png",
 ];
 
+/**
+ * Play the idle animation of enemy of type `Y-Wing`
+ * @returns {void}
+ */
 function animateYWingEnemy(enemy) {
   animation.animate(
     "yWingEnemyIdle",
@@ -278,6 +294,10 @@ const enemyXWingIdleAnimations = [
   "assets/Enemies/X-Wing/0005.png",
 ];
 
+/**
+ * Play the idle animation of enemy of type `X-Wing`
+ * @returns {void}
+ */
 function animateXWingEnemy(enemy) {
   animation.animate(
     "xWingEnemyIdle",
@@ -302,6 +322,10 @@ const enemyXWingDamageAnimations = [
   "assets/Enemies/X-Wing/0000.png",
 ];
 
+/**
+ * Play the damage animation of enemy of type `X-Wing`
+ * @returns {void}
+ */
 function animateXWingEnemyDamage(index) {
   animation.animate(
     "xWingEnemyDamage",
@@ -328,6 +352,10 @@ const enemyYWingDamageAnimations = [
   "assets/Enemies/Y-Wing/0000.png",
 ];
 
+/**
+ * Play the damage animation of enemy of type `Y-Wing`
+ * @returns {void}
+ */
 function animateYWingEnemyDamage(index) {
   animation.animate(
     "yWingEnemyDamage",
