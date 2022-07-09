32ee191162115856b4d24516030a862c8a752968
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-07-09 19:12:08 -0300

    refact scoreController

--- controllers/scoreController.js
+++ controllers/scoreController.js
@@ -2,301 +2,206 @@ import GAME_SETTINGS from "../constants/gameSettings.js";
 import LOCAL_STORAGE_KEYS from "../constants/localStorageKeys.js";
 
 export default {
-    addPoint: function (numberOfPoints = 1) {
-        GAME_SETTINGS.RECORD += numberOfPoints;
-    },
-    addName: function(playerName){
-        GAME_SETTINGS.RECORD_NAME = playerName;
-    },
-    verifyNewScore: function(){
-        if (GAME_SETTINGS.RECORD <= Number(localStorage.fifth)){
-            GAME_SETTINGS.NEW_SCORE = false;
-        }
-        else{
-            GAME_SETTINGS.NEW_SCORE = true;
-        }
-    },
-    verifyCurrentScore: function() {
-        debugger;
-        if (GAME_SETTINGS.RECORD <= Number(localStorage.fifth)) return;
+  /**
+   * Add points to the player's record
+   * @param {number} numberOfPoints Number of points to be added on player's record. Default is `1`
+   */
+  addPoint: function (numberOfPoints = 1) {
+    GAME_SETTINGS.RECORD += numberOfPoints;
+  },
+  /**
+   * Set the player name
+   * @param {string} playerName Player's name
+   */
+  addName: function (playerName) {
+    GAME_SETTINGS.RECORD_NAME = playerName;
+  },
+  verifyNewScore: function () {
+    const fifthRecord = Number(
+      localStorage.getItem(LOCAL_STORAGE_KEYS.FIFTH_RECORD)
+    );
 
-        else if(GAME_SETTINGS.RECORD > Number(localStorage.first))
-        {
-            //Record
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIFTH_RECORD,
-                localStorage.forth
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FORTH_RECORD,
-                localStorage.third
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.THIRD_RECORD,
-                localStorage.second
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.SECOND_RECORD,
-                localStorage.first
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIRST_RECORD,
-                GAME_SETTINGS.RECORD
-            )
+    GAME_SETTINGS.NEW_SCORE = GAME_SETTINGS.RECORD <= fifthRecord;
+  },
+  verifyCurrentScore: function () {
+    if (GAME_SETTINGS.RECORD <= Number(localStorage.fifth)) return;
+    else if (GAME_SETTINGS.RECORD > Number(localStorage.first)) {
+      //Record
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FIFTH_RECORD, localStorage.forth);
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FORTH_RECORD, localStorage.third);
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.THIRD_RECORD,
+        localStorage.second
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.SECOND_RECORD,
+        localStorage.first
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FIRST_RECORD,
+        GAME_SETTINGS.RECORD
+      );
 
-            //Name record
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIFTH_NAME,
-                localStorage.forthName
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FORTH_NAME,
-                localStorage.thirdName
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.THIRD_NAME,
-                localStorage.secondName
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.SECOND_NAME,
-                localStorage.firstName
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIRST_NAME,
-                GAME_SETTINGS.RECORD_NAME
-            )
+      //Name record
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FIFTH_NAME,
+        localStorage.forthName
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FORTH_NAME,
+        localStorage.thirdName
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.THIRD_NAME,
+        localStorage.secondName
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.SECOND_NAME,
+        localStorage.firstName
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FIRST_NAME,
+        GAME_SETTINGS.RECORD_NAME
+      );
+    } else if (GAME_SETTINGS.RECORD > Number(localStorage.second)) {
+      //Record
 
-        }
-        else if(GAME_SETTINGS.RECORD > Number(localStorage.second))
-        {
-            //Record
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FIFTH_RECORD, localStorage.forth);
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FORTH_RECORD, localStorage.third);
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.THIRD_RECORD,
+        localStorage.second
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.SECOND_RECORD,
+        GAME_SETTINGS.RECORD
+      );
 
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIFTH_RECORD,
-                localStorage.forth
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FORTH_RECORD,
-                localStorage.third
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.THIRD_RECORD,
-                localStorage.second
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.SECOND_RECORD,
-                GAME_SETTINGS.RECORD
-            )
+      //Name Record
 
-            //Name Record
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FIFTH_NAME,
+        localStorage.forthName
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FORTH_NAME,
+        localStorage.thirdName
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.THIRD_NAME,
+        localStorage.secondName
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.SECOND_NAME,
+        GAME_SETTINGS.RECORD_NAME
+      );
+    } else if (GAME_SETTINGS.RECORD > Number(localStorage.third)) {
+      //Record
 
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIFTH_NAME,
-                localStorage.forthName
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FORTH_NAME,
-                localStorage.thirdName
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.THIRD_NAME,
-                localStorage.secondName
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.SECOND_NAME,
-                GAME_SETTINGS.RECORD_NAME
-            )
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FIFTH_RECORD, localStorage.forth);
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FORTH_RECORD, localStorage.third);
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.THIRD_RECORD,
+        GAME_SETTINGS.RECORD
+      );
 
-        }
-        else if(GAME_SETTINGS.RECORD > Number(localStorage.third))
-        {
-            //Record
+      //Name Record
 
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIFTH_RECORD,
-                localStorage.forth
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FORTH_RECORD,
-                localStorage.third
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.THIRD_RECORD,
-                GAME_SETTINGS.RECORD
-            )
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FIFTH_NAME,
+        localStorage.forthName
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FORTH_NAME,
+        localStorage.thirdName
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.THIRD_NAME,
+        GAME_SETTINGS.RECORD_NAME
+      );
+    } else if (GAME_SETTINGS.RECORD > Number(localStorage.forth)) {
+      //Record
 
-            //Name Record
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FIFTH_RECORD, localStorage.forth);
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FORTH_RECORD,
+        GAME_SETTINGS.RECORD
+      );
 
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIFTH_NAME,
-                localStorage.forthName
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FORTH_NAME,
-                localStorage.thirdName
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.THIRD_NAME,
-                GAME_SETTINGS.RECORD_NAME
-            )
+      //Record Name
 
-        }
-        else if(GAME_SETTINGS.RECORD > Number(localStorage.forth))
-        {
-            //Record
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FIFTH_NAME,
+        localStorage.forthName
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FORTH_NAME,
+        GAME_SETTINGS.RECORD_NAME
+      );
+    } else if (GAME_SETTINGS.RECORD > Number(localStorage.fifth)) {
+      //Record
 
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIFTH_RECORD,
-                localStorage.forth
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FORTH_RECORD,
-                GAME_SETTINGS.RECORD
-            )
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FIFTH_RECORD,
+        GAME_SETTINGS.RECORD
+      );
 
-            //Record Name
+      //Name Record
 
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIFTH_NAME,
-                localStorage.forthName
-            )
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FORTH_NAME,
-                GAME_SETTINGS.RECORD_NAME
-            )
+      localStorage.setItem(
+        LOCAL_STORAGE_KEYS.FIFTH_NAME,
+        GAME_SETTINGS.RECORD_NAME
+      );
+    }
+  },
+  verifyLocalScore: function () {
+    //Record
 
-        }
-        else if(GAME_SETTINGS.RECORD > Number(localStorage.fifth))
-        {
-            //Record
-
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIFTH_RECORD,
-                GAME_SETTINGS.RECORD
-            )
-
-            //Name Record
-
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIFTH_NAME,
-                GAME_SETTINGS.RECORD_NAME
-            )
-            
-        }
-    },
-    verifyLocalScore: function () {
-        //Record
-
-        if(localStorage.first == null){
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIRST_RECORD,
-                0
-            )
-        }
-        if(localStorage.second == null){
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.SECOND_RECORD,
-                0
-            )
-        }
-        if(localStorage.third == null){
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.THIRD_RECORD,
-                0
-            )
-        }
-        if(localStorage.forth == null){
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FORTH_RECORD,
-                0
-            )
-        }
-        if(localStorage.fifth == null){
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIFTH_RECORD,
-                0
-            )
-        }
-
-        //Name Record
-
-        if(localStorage.firstName == null){
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIRST_NAME,
-                "..."
-            )
-        }
-        if(localStorage.secondName == null){
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.SECOND_NAME,
-                "..."
-            )
-        }
-        if(localStorage.thirdName == null){
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.THIRD_NAME,
-                "..."
-            )
-        }
-        if(localStorage.forthName == null){
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FORTH_NAME,
-                "..."
-            )
-        }
-        if(localStorage.fifthName == null){
-            localStorage.setItem(
-                LOCAL_STORAGE_KEYS.FIFTH_NAME,
-                "..."
-            )
-        }
-    },
-    resetLocalScore: function(){
-        //Record
-
-        localStorage.setItem(
-            LOCAL_STORAGE_KEYS.FIRST_RECORD,
-            0
-        )
-        localStorage.setItem(
-            LOCAL_STORAGE_KEYS.SECOND_RECORD,
-            0
-        )
-        localStorage.setItem(
-            LOCAL_STORAGE_KEYS.THIRD_RECORD,
-            0
-        )
-        localStorage.setItem(
-            LOCAL_STORAGE_KEYS.FORTH_RECORD,
-            0
-        )
-        localStorage.setItem(
-            LOCAL_STORAGE_KEYS.FIFTH_RECORD,
-            0
-        )
-
-        //Name Record
-
-        localStorage.setItem(
-            LOCAL_STORAGE_KEYS.FIRST_NAME,
-            "..."
-        )
-        localStorage.setItem(
-            LOCAL_STORAGE_KEYS.SECOND_NAME,
-            "..."
-        )
-        localStorage.setItem(
-            LOCAL_STORAGE_KEYS.THIRD_NAME,
-            "..."
-        )
-        localStorage.setItem(
-            LOCAL_STORAGE_KEYS.FORTH_NAME,
-            "..."
-        )
-        localStorage.setItem(
-            LOCAL_STORAGE_KEYS.FIFTH_NAME,
-            "..."
-        )
+    if (localStorage.first == null) {
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FIRST_RECORD, 0);
+    }
+    if (localStorage.second == null) {
+      localStorage.setItem(LOCAL_STORAGE_KEYS.SECOND_RECORD, 0);
+    }
+    if (localStorage.third == null) {
+      localStorage.setItem(LOCAL_STORAGE_KEYS.THIRD_RECORD, 0);
+    }
+    if (localStorage.forth == null) {
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FORTH_RECORD, 0);
+    }
+    if (localStorage.fifth == null) {
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FIFTH_RECORD, 0);
     }
 
+    //Name Record
+
+    if (localStorage.firstName == null) {
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FIRST_NAME, "...");
+    }
+    if (localStorage.secondName == null) {
+      localStorage.setItem(LOCAL_STORAGE_KEYS.SECOND_NAME, "...");
+    }
+    if (localStorage.thirdName == null) {
+      localStorage.setItem(LOCAL_STORAGE_KEYS.THIRD_NAME, "...");
+    }
+    if (localStorage.forthName == null) {
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FORTH_NAME, "...");
+    }
+    if (localStorage.fifthName == null) {
+      localStorage.setItem(LOCAL_STORAGE_KEYS.FIFTH_NAME, "...");
+    }
+  },
+  resetLocalScore: function () {
+    localStorage.setItem(LOCAL_STORAGE_KEYS.FIRST_RECORD, 0);
+    localStorage.setItem(LOCAL_STORAGE_KEYS.SECOND_RECORD, 0);
+    localStorage.setItem(LOCAL_STORAGE_KEYS.THIRD_RECORD, 0);
+    localStorage.setItem(LOCAL_STORAGE_KEYS.FORTH_RECORD, 0);
+    localStorage.setItem(LOCAL_STORAGE_KEYS.FIFTH_RECORD, 0);
+
+    localStorage.setItem(LOCAL_STORAGE_KEYS.FIRST_NAME, "...");
+    localStorage.setItem(LOCAL_STORAGE_KEYS.SECOND_NAME, "...");
+    localStorage.setItem(LOCAL_STORAGE_KEYS.THIRD_NAME, "...");
+    localStorage.setItem(LOCAL_STORAGE_KEYS.FORTH_NAME, "...");
+    localStorage.setItem(LOCAL_STORAGE_KEYS.FIFTH_NAME, "...");
+  },
 };
